84d8f75ecdcb02579a4e6a9d3425fcc7a1712ea2
Nikolai_Boravlev <120275954+nboravlev@users.noreply.github.com>
2025-05-23 16:56:27 +0300

    Update train_val_split.py
    train test val

--- train_val_split.py
+++ train_val_split.py
@@ -1,4 +1,4 @@
-# Разделить данные на Train и Test
+# Разделить данные на Train, Validation и Test
 
 from pathlib import Path
 import random
@@ -14,22 +14,27 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--datapath', help='Указать путь к данным',
                     required=True)
 parser.add_argument('--train_pct', help='Доля данных, которая идет на обучение; \
-                    Остельные данные сохраняются в папке для валидации (пример: ".8")',
+                   (пример: ".8")',
                     default=.8)
+
+parser.add_argument('--test_pct', help='Доля данных, которая идет на test; \
+                    Остельные данные сохраняются в папке для валидации (пример: ".05")',
+                    default=.05)
 
 args = parser.parse_args()
 
 data_path = args.datapath
 train_percent = float(args.train_pct)
+test_persent = float(args.test_pct)
 
 # Check for valid entries
 if not os.path.isdir(data_path):
    print('Directory specified by --datapath not found. Verify the path is correct (and uses double back slashes if on Windows) and try again.')
    sys.exit(0)
-if train_percent < .01 or train_percent > 0.99:
-   print('Invalid entry for train_pct. Please enter a number between .01 and .99.')
+if train_percent < .01 or train_percent > 0.9:
+   print('Invalid entry for train_pct. Please enter a number between .01 and .9.')
    sys.exit(0)
-val_percent = 1 - train_percent
+val_percent = 1 - train_percent - test_persent
 
 # Define path to input dataset 
 input_image_path = os.path.join(data_path,'images')
@@ -41,6 +46,8 @@ train_img_path = os.path.join(cwd,'data/train/images')
 train_txt_path = os.path.join(cwd,'data/train/labels')
 val_img_path = os.path.join(cwd,'data/validation/images')
 val_txt_path = os.path.join(cwd,'data/validation/labels')
+test_img_path = os.path.join(cwd,'data/test/images')
+test_txt_path = os.path.join(cwd,'data/test/labels')
 
 # Create folders if they don't already exist
 for dir_path in [train_img_path, train_txt_path, val_img_path, val_txt_path]:
@@ -59,12 +66,14 @@ print(f'Number of annotation files: {len(txt_file_list)}')
 # Determine number of files to move to each folder
 file_num = len(img_file_list)
 train_num = int(file_num*train_percent)
-val_num = file_num - train_num
+test_num = int(file_num*test_percent)
+val_num = file_num - train_num - test_num
 print('Images moving to train: %d' % train_num)
+print('Images moving to test: %d' % test_num)
 print('Images moving to validation: %d' % val_num)
 
 # Select files randomly and copy them to train or val folders
-for i, set_num in enumerate([train_num, val_num]):
+for i, set_num in enumerate([train_num, val_num, test_num]):
   for ii in range(set_num):
     img_path = random.choice(img_file_list)
     img_fn = img_path.name
@@ -76,6 +85,8 @@ for i, set_num in enumerate([train_num, val_num]):
       new_img_path, new_txt_path = train_img_path, train_txt_path
     elif i == 1: # Copy second set of files to the validation folders
       new_img_path, new_txt_path = val_img_path, val_txt_path
+    elif i == 2: # Copy second set of files to the test folders
+    new_img_path, new_txt_path = test_img_path, test_txt_path
 
     shutil.copy(img_path, os.path.join(new_img_path,img_fn))
     #os.rename(img_path, os.path.join(new_img_path,img_fn))
