d4437d5e5003b38207d31c107a2cd0f3910cbcc0
Nikolai_Boravlev <120275954+nboravlev@users.noreply.github.com>
2025-05-23 17:00:30 +0300

    Update train_val_split.py

--- train_val_split.py
+++ train_val_split.py
@@ -83,7 +83,7 @@ for i, set_num in enumerate([train_num, val_num, test_num]):
 
     if i == 0: # Copy first set of files to train folders
       new_img_path, new_txt_path = train_img_path, train_txt_path
-    elif i == 1: # Copy second set of files to the validation folders
+    if i == 1: # Copy second set of files to the validation folders
       new_img_path, new_txt_path = val_img_path, val_txt_path
     elif i == 2: # Copy second set of files to the test folders
     new_img_path, new_txt_path = test_img_path, test_txt_path
