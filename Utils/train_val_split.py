8af20b8ee4d995fa60a1adad3329b6b3bafd9554
Nikolai_Boravlev <120275954+nboravlev@users.noreply.github.com>
2025-05-24 15:28:40 +0300

    Update train_val_split.py

--- Utils/train_val_split.py
+++ Utils/train_val_split.py
@@ -16,20 +16,24 @@ parser.add_argument('--datapath', help='Path to data folder containing image and
 parser.add_argument('--train_pct', help='Ratio of images to go to train folder; \
                     the rest go to validation folder (example: ".8")',
                     default=.8)
+parser.add_argument('--test_pct', help='Ratio of images to go to test folder; \
+                    the rest go to validation folder (example: ".1")',
+                    default=.05)
 
 args = parser.parse_args()
 
 data_path = args.datapath
 train_percent = float(args.train_pct)
+test_percent = float(args.test_pct)
 
 # Check for valid entries
 if not os.path.isdir(data_path):
    print('Directory specified by --datapath not found. Verify the path is correct (and uses double back slashes if on Windows) and try again.')
    sys.exit(0)
-if train_percent < .01 or train_percent > 0.99:
+if train_percent < .01 or train_percent > 0.9:
    print('Invalid entry for train_pct. Please enter a number between .01 and .99.')
    sys.exit(0)
-val_percent = 1 - train_percent
+val_percent = 1 - train_percent - test_percent
 
 # Define path to input dataset 
 input_image_path = os.path.join(data_path,'images')
@@ -39,11 +43,13 @@ input_label_path = os.path.join(data_path,'labels')
 cwd = os.getcwd()
 train_img_path = os.path.join(cwd,'data/train/images')
 train_txt_path = os.path.join(cwd,'data/train/labels')
+test_img_path = os.path.join(cwd,'data/test/images')
+test_txt_path = os.path.join(cwd,'data/test/labels')
 val_img_path = os.path.join(cwd,'data/validation/images')
 val_txt_path = os.path.join(cwd,'data/validation/labels')
 
 # Create folders if they don't already exist
-for dir_path in [train_img_path, train_txt_path, val_img_path, val_txt_path]:
+for dir_path in [train_img_path, train_txt_path, val_img_path, val_txt_path, test_img_path, test_txt_path]:
    if not os.path.exists(dir_path):
       os.makedirs(dir_path)
       print(f'Created folder at {dir_path}.')
@@ -59,8 +65,10 @@ print(f'Number of annotation files: {len(txt_file_list)}')
 # Determine number of files to move to each folder
 file_num = len(img_file_list)
 train_num = int(file_num*train_percent)
-val_num = file_num - train_num
+test_num = int(file_num*test_percent)
+val_num = file_num - train_num - test_num
 print('Images moving to train: %d' % train_num)
+print('Images moving to test: %d' % test_num)
 print('Images moving to validation: %d' % val_num)
 
 # Select files randomly and copy them to train or val folders
@@ -72,10 +80,12 @@ for i, set_num in enumerate([train_num, val_num]):
     txt_fn = base_fn + '.txt'
     txt_path = os.path.join(input_label_path,txt_fn)
 
-    if i == 0: # Copy first set of files to train folders
-      new_img_path, new_txt_path = train_img_path, train_txt_path
-    elif i == 1: # Copy second set of files to the validation folders
-      new_img_path, new_txt_path = val_img_path, val_txt_path
+    if i == 0:
+        new_img_path, new_txt_path = train_img_path, train_txt_path
+    elif i == 1:
+        new_img_path, new_txt_path = test_img_path, test_txt_path
+    elif i == 2:
+        new_img_path, new_txt_path = val_img_path, val_txt_path
 
     shutil.copy(img_path, os.path.join(new_img_path,img_fn))
     #os.rename(img_path, os.path.join(new_img_path,img_fn))
